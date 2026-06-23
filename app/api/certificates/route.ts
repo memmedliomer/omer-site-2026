959c639e5ac4c662f852bcae607b89a51b0d290f
Omar Mammadli <158719141+memmedliomer@users.noreply.github.com>
2026-06-24 01:10:38 +0400

    Update route.ts

--- app/api/certificates/route.ts
+++ app/api/certificates/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { db } from '../../lib/db';
-
+export const dynamic = 'force-dynamic';
 export async function GET() {
   try {
     // Sertifikatları və aid olduqları qrupun adını bazadan çəkirik
